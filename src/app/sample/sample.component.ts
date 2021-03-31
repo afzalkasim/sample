8618b0ea27fff17dfff4dc4d4cc181734cf948d8
suresh@rubixtek.com <suresh@rubixtek.com>
2021-03-31 11:57:42 +0530

    sample

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 // import { User } from '../models/user';
 
 @Component({
@@ -7,14 +8,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sample.component.scss']
 })
 export class SampleComponent implements OnInit {
-// register=new User();
-// name:any=""
-  constructor() { }
+
+  reactiveForm: FormGroup;
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+   this.reactiveForm = new FormGroup({
+    //  name : [null, [Validators.required, Validators.minLength(2)]],
+    //  gender: [null, [Validators.required]],
+    //  Dob: [null, [Validators.required]],
+    //  city: [null, [Validators.required]],
+    //  email: [null, [Validators.required]],
+    //  address: [null, [Validators.required]],
+    //  contactnumber: [null, [Validators.required]]
+    name: new FormControl("", [Validators.required]),
+    gender: new FormControl("", [Validators.required]),
+    Dob: new FormControl("", [Validators.required]),
+    city: new FormControl("", [Validators.required]),
+    email: new FormControl("", [Validators.required]),
+    address: new FormControl("", [Validators.required]),
+    contactnumber: new FormControl("", [Validators.required])
+   })
   }
-  onSubmit(myForm:any){
-    console.log(myForm);
-    console.log("value is",myForm.value);
+ get name(){
+   return this.reactiveForm.get("name")
+  }
+  onSubmit(){
+    console.log(this.reactiveForm);
+    // console.log("value is",this.reactiveForm.value);
   }
 }
