5cbeb5f3b8e06b1e12bfc98c9c016ce2a65f763a
suresh@rubixtek.com <suresh@rubixtek.com>
2021-03-31 16:47:42 +0530

    sample done

--- src/app/sample/sample.component.ts
+++ src/app/sample/sample.component.ts
@@ -13,28 +13,27 @@ export class SampleComponent implements OnInit {
   constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
-   this.reactiveForm = new FormGroup({
-    //  name : [null, [Validators.required, Validators.minLength(2)]],
-    //  gender: [null, [Validators.required]],
-    //  Dob: [null, [Validators.required]],
-    //  city: [null, [Validators.required]],
-    //  email: [null, [Validators.required]],
-    //  address: [null, [Validators.required]],
-    //  contactnumber: [null, [Validators.required]]
-    name: new FormControl("", [Validators.required]),
-    gender: new FormControl("", [Validators.required]),
-    Dob: new FormControl("", [Validators.required]),
-    city: new FormControl("", [Validators.required]),
-    email: new FormControl("", [Validators.required]),
-    address: new FormControl("", [Validators.required]),
-    contactnumber: new FormControl("", [Validators.required])
+   this.reactiveForm = this._fb.group({
+     name : ["", [Validators.required, Validators.pattern(/^[A-Za-z]+$/)]],
+     gender: ["", [Validators.required]],
+     Dob: ["", [Validators.required]],
+     city: ["", [Validators.required]],
+     email: ["", [Validators.required]],
+     address: ["", [Validators.required]],
+     contactnumber: ["", [Validators.required , Validators.pattern(/^\d*$/)]]
+    // name: new FormControl("", [Validators.required]),
+    // gender: new FormControl("", [Validators.required]),
+    // Dob: new FormControl("", [Validators.required]),
+    // city: new FormControl("", [Validators.required]),
+    // email: new FormControl("", [Validators.required]),
+    // address: new FormControl("", [Validators.required]),
+    // contactnumber: new FormControl("", [Validators.required])
    })
   }
- get name(){
-   return this.reactiveForm.get("name")
-  }
+ 
   onSubmit(){
     console.log(this.reactiveForm);
-    // console.log("value is",this.reactiveForm.value);
+    console.log("value is",this.reactiveForm.value);
+    alert("Successfully submitted")
   }
 }
